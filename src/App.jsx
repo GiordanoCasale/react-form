8895656e35ee10e40829fa19ada6fed65bc852a0
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-09 15:59:06 +0200

    [UPD] add message display for game addition and removal

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     "Minecraft",
     "Grand Theft Auto V",
     "The Witcher 3",
-    "Pokemon",
+    "Pokémon",
     "Red Dead Redemption 2",
     "Dark Souls",
     "Half-Life"
@@ -18,29 +18,42 @@ function App() {
   //dichiaro una variabile per aggiungere un nuovo gioco all'array
   const [videoGames, setVideoGames] = useState(Games)
   const [newGame, setNewGame] = useState("")
+  const [message, setMessage] = useState("")// stato per l'apparizione del messaggio
+  const [messageType, setMessageType] = useState(""); //stato per la differenziazione del messaggio
 
   //dichiaro una variabile per impedire il ricarimento della pagina
   const addGame = e => {
     e.preventDefault();
-    //variabile per far aggiornare correttamente la ui
     const updatedVideoGames = [...videoGames, newGame];
     setVideoGames(updatedVideoGames);
-    setNewGame("");//questo lo usiamo per ripulire il form
+    setMessage(`Il gioco "${newGame}" è stato aggiunto alla lista!`);
+    setMessageType("success"); //serve per differenziare il tipo di messaggio, in questo caso differenziare i colori
+    setNewGame(""); //per tenere pulito il form
+    setTimeout(() => {
+      setMessage("");
+      setMessageType(""); // resetta anche il tipo
+    }, 3000);
   }
   //variabile per l'eliminazione di un elemento nell'array e la creazione del clone senza il suddetto elemento
   const removeGame = i => {
+    const gameToRemove = videoGames[i];
     const updatedGames = videoGames.filter((game, index) => {
       return index !== i
     });
     setVideoGames(updatedGames);
+    setMessage(`Il gioco "${gameToRemove}" è stato rimosso dalla lista!`);
+    setMessageType("error"); // serve per differenziare il tipo di messaggio, in questo caso differenziare i colori
+    setTimeout(() => {
+      setMessage("");
+      setMessageType(""); // resetta anche il tipo
+    }, 3000);
   }
 
   return (
-    //vado a mettere il titolo e a ciclare l'array col metodo map
     <div className='container'>
       <h1>Lista Videogiochi</h1>
       <ul>
-        {videoGames.map((game, index) => (
+        {videoGames.map((game, index) => ( //andiamo a ciclare l'array con map e crearne una copia
           <li key={index}>
             <span>{game}</span>
             <button className='remove-button' onClick={() => removeGame(index)}>
@@ -54,9 +67,10 @@ function App() {
         <input className='input' type="text" value={newGame} onChange={e => { setNewGame(e.target.value) }} />
         <button className='btn'>Aggiungi Gioco</button>
       </form>
+      {message && <div className={`message ${messageType}`}>{message}</div> // rendering per la differenziazione dei messaggi
+      }
     </div>
   )
-
 }
 
 export default App
