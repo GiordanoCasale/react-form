3fdd14a5c5d3a026271cb2efad2062028c7e2006
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-09 14:35:26 +0200

    [UPD] first milestone completata

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import { useState } from 'react'
 
 function App() {
+  const videoGames = [
+    "The Legend of Zelda",
+    "Super Mario Bros",
+    "Final Fantasy VII",
+    "Minecraft",
+    "Grand Theft Auto V",
+    "The Witcher 3",
+    "Pokemon",
+    "Red Dead Redemption 2",
+    "Dark Souls",
+    "Half-Life"
+  ];
+  //dichiaro una variabile per aggiungere un nuovo gioco all'array
+
+  const [newGame, setNewGame] = useState("")
+
+  //dichiaro una variabile per impedire il ricarimento della pagina
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('Il gioco aggiunto è:' + newGame)
+  }
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    //vado a mettere il titolo e a ciclare l'array col metodo map
+    <div className='container'>
+      <h1>Lista Videogiochi</h1>
+      <ul>
+        {videoGames.map((game, index) => (
+          <li key={index}>{game}</li>
+        ))}
+      </ul>
+
+      <form className='form' onSubmit={handleSubmit}>
+        <input className='input' type="text" value={newGame} onChange={e => { setNewGame(e.target.value) }} />
+        <button className='btn'>Aggiungi Gioco</button>
+      </form>
     </div>
   )
+
 }
 
 export default App
